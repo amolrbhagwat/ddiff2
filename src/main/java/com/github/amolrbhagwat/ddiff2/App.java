6d801b83e348a2dcdfc9fde82ac0a260017b3fed
Amol Bhagwat <amolrbhagwat@gmail.com>
2019-05-29 18:05:28 -0400

    Add basic JavaFX stage

--- src/main/java/com/github/amolrbhagwat/ddiff2/App.java
+++ src/main/java/com/github/amolrbhagwat/ddiff2/App.java
@@ -1,13 +1,31 @@
 package com.github.amolrbhagwat.ddiff2;
 
-/**
- * Hello world!
- *
- */
-public class App
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+		loader.setController(this);
+		Parent root = loader.load();
+
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		stage = primaryStage;
+	}
 }
