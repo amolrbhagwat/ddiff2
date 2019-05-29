29c4ec50014c6144ab850685aceacaa6eda2f83c
Amol Bhagwat <amolrbhagwat@gmail.com>
2019-05-29 18:05:53 -0400

    Add code to select and set directories

--- src/main/java/com/github/amolrbhagwat/ddiff2/App.java
+++ src/main/java/com/github/amolrbhagwat/ddiff2/App.java
@@ -1,16 +1,50 @@
 package com.github.amolrbhagwat.ddiff2;
 
+import java.io.File;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 
 public class App extends Application
 {
+	@FXML Button checkButton;
+	@FXML TextField sourceDirTextView;
+	@FXML TextField targetDirTextView;
+
+	Stage stage;
+
+	File sourceDirectory;
+	File targetDirectory;
+
+	@FXML
+	public void setSourceDirectory(ActionEvent event) {
+		File selectedDirectory = new DirectoryChooser().showDialog(stage);
+
+		if(selectedDirectory != null) {
+			sourceDirectory = selectedDirectory;
+			sourceDirTextView.setText(sourceDirectory.toString());
+		}
+	}
+
+	@FXML
+	public void setTargetDirectory(ActionEvent event) {
+		File selectedDirectory = new DirectoryChooser().showDialog(stage);
+
+		if(selectedDirectory != null) {
+			targetDirectory = selectedDirectory;
+			targetDirTextView.setText(targetDirectory.toString());
+		}
+	}
+
     public static void main( String[] args )
     {
         launch(args);
