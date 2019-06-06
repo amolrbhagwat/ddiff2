4c92b4dab6a0b06b709c5579d560360a031d8628
Amol Bhagwat <amolrbhagwat@gmail.com>
2019-06-05 20:42:46 -0400

    Configure table columns and add code to set results

--- src/main/java/com/github/amolrbhagwat/ddiff2/App.java
+++ src/main/java/com/github/amolrbhagwat/ddiff2/App.java
@@ -1,14 +1,19 @@
 package com.github.amolrbhagwat.ddiff2;
 
 import java.io.File;
+import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
@@ -20,10 +25,41 @@ public class App extends Application
 	@FXML TextField sourceDirTextView;
 	@FXML TextField targetDirTextView;
 
+	@FXML TableView<DiffResult> resultsTableView;
+	@FXML TableColumn<DiffResult, String> filenameColumn;
+	@FXML TableColumn<DiffResult, String> statusColumn;
+	@FXML TableColumn<DiffResult, String> commentColumn;
+
 	Stage stage;
 
 	File sourceDirectory;
 	File targetDirectory;
+
+	private ObservableList<DiffResult> diffResults;
+
+	@FXML
+	public void diff(ActionEvent event) throws IOException {
+		Index index = new Index(targetDirectory);
+
+		diffResults.setAll(DirectoryDiffer.diff(sourceDirectory, index));
+	}
+
+	@FXML
+    private void initialize() {
+		filenameColumn.setCellValueFactory(cellData ->
+			cellData.getValue().filenameProperty());
+		statusColumn.setCellValueFactory(cellData ->
+			cellData.getValue().statusProperty());
+		commentColumn.setCellValueFactory(cellData ->
+			cellData.getValue().commentProperty());
+
+		diffResults = FXCollections.observableArrayList();
+		resultsTableView.setItems(diffResults);
+    }
+
+	public ObservableList<DiffResult> getDiffResults() {
+        return diffResults;
+    }
 
 	@FXML
 	public void setSourceDirectory(ActionEvent event) {
@@ -36,7 +72,7 @@ public class App extends Application
 	}
 
 	@FXML
-	public void setTargetDirectory(ActionEvent event) {
+	public void setTargetDirectory(ActionEvent event) throws IOException {
 		File selectedDirectory = new DirectoryChooser().showDialog(stage);
 
 		if(selectedDirectory != null) {
@@ -45,8 +81,7 @@ public class App extends Application
 		}
 	}
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 
